d84f9349e0217cbdc2d1c6cffc57cec37ca8cd9c
Eric Mittelhammer <code@ericmittelhammer.com>
2015-04-03 17:14:48 -0400

    Create Main.hs

---
+++ fibonacci/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+-- |Generates the first n numbers in the fibonacci sequence
+-- returned sequence is in reverse, of course, becasue cons
+fib :: Int -> [Int]
+fib 0 = [0]
+fib 1 = [1,0]
+fib n = sum (take 2 xs) : xs
+    where xs = fib $ n - 1
+
+main = do
+  n <- getLine
+  print $ fib $ read n
