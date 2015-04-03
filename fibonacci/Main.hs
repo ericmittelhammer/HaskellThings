7a43f71b98a2881e67db7fed088f2606e7dd8d1b
Eric Mittelhammer <code@ericmittelhammer.com>
2015-04-03 17:15:07 -0400

    Update Main.hs

--- fibonacci/Main.hs
+++ fibonacci/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 -- |Generates the first n numbers in the fibonacci sequence
--- returned sequence is in reverse, of course, becasue cons
+-- returned sequence is in reverse, of course, because cons
 fib :: Int -> [Int]
 fib 0 = [0]
 fib 1 = [1,0]
